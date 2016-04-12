1638454b233fda41cc98c9dc1cfd63d96b094148
孙槐 <sunhuai@zplay.cn>
2016-04-12 18:42:37 +0800

    beta debug

--- apps/test/controllers/Test.php
+++ apps/test/controllers/Test.php
@@ -4,6 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Test extends WhaleController {
     public function index()
     {
-        $this->load->view('welcome_message');
+        $this->template->display('welcome.tpl');
     }
 }
